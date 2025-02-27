b4455167326c700401c72a785ccf794c774d1e74
lleroc <llleroc@yahoo.es>
2025-02-26 20:10:44 -0500

    variables2

---
+++ Semana 3/variables.py
@@ -0,0 +1,32 @@
+#calculadora
+#n1 = 10     #estaba quemando codigo
+#n2 = 5
+#suma = n1+n2
+#resta = n1-n2
+#multiplicacion = n1*n2
+#division = n1/n2
+#print("El resultaddo de la suma es: " + str(suma))
+#print("El resultaddo de la resta es: " + str(resta))
+#print("El resultaddo de la multiplicacion es: " + str(multiplicacion))
+#print("El resultaddo de la division es: " + str(division))
+#metodos de conversion de datos
+#de numero a texto 
+#str(solo numeros)
+#de texto a numero
+#int(solo letras)
+
+#f string cadana de formato
+#print(f"El resultaddo de la suma es: {suma}")
+#print(f"El resultaddo de la resta es: {resta}")
+#print(f"El resultaddo de la multiplicacion es: {multiplicacion}")
+#print(f"El resultaddo de la division es: {division}")
+
+#ingreso por teclado los valores
+n1 = input()
+n2 = 5
+suma = n1 + n2
+resta = n1 - n2
+multiplicacion = n1 * n2
+division = n1 / n2
+
+
